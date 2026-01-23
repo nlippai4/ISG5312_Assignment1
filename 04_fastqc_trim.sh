4c44dfbe96c61b14d769735f45ee059e304f933b
Nora Lippai <nlippai@hpc-ext-2.cam.uchc.edu>
2026-01-23 15:03:42 -0500

    git add new 04_fastqc_trim.sh

--- 04_fastqc_trim.sh
+++ 04_fastqc_trim.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=fastqc_trim
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 10
-#SBATCH --mem=20G
+#SBATCH -c 12
+#SBATCH --mem=25G
 #SBATCH --partition=general
 #SBATCH --qos=general
 #SBATCH --mail-type=ALL
@@ -26,6 +26,6 @@ REPORTDIR=../../results/02_qc/fastqc_trimmed_reports
 mkdir -p $REPORTDIR
 
 ACCLIST=../../metadata/accessionlist.txt
-# run fastp in parallel, 10 samples at a time
-cat $ACCLIST | parallel -j 10 \
+# run fastp in parallel, 12 samples at a time
+cat $ACCLIST | parallel -j 12 \
     "fastqc --outdir $REPORTDIR $INDIR/{}_trim_{1..2}.fastq.gz"
